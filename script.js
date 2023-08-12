a53655b1d5aad29268027120d0ec691cc3102b41
AVISHEK OJHA <140743024+czabhishek699@users.noreply.github.com>
2023-08-12 20:21:02 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get a reference to the button element
+const button = document.getElementById('constructionButton');
+
+// Add a click event listener to the button
+button.addEventListener('click', () => {
+    const message = "Feature under construction. For any queries Mail - avishekojha66@gmail.com";
+    alert(message);
+});
